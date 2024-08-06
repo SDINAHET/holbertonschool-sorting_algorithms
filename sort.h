d2448fd60869b2642ba9c8e7d3f27d82f12a9018
SDINAHET <stephane.dinahet@gmail.com>
2024-08-06 20:52:18 +0200

    fix sort.h

--- sort.h
+++ sort.h
@@ -10,16 +10,16 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 /*print_array.c*/
 void print_list(const listint_t *list);
 
 /*print_list.c*/
-void print_array(const int *array, size_t size)
+void print_array(const int *array, size_t size);
 
 /*prototype*/
 
