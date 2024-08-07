9330c40bdf3fb69aa286bbcc22d6aa9700c95d91
SDINAHET <stephane.dinahet@gmail.com>
2024-08-08 01:53:09 +0200

    fix ; sort.h

--- sort.h
+++ sort.h
@@ -36,8 +36,8 @@ void swap(int *array, size_t i, size_t j);
 
 /*3*/
 void quick_sort(int *array, size_t size);
-void swap(int *array, size_t i, size_t j)
-size_t lomuto_partition(int *array, size_t low, size_t high)
+void swap(int *array, size_t i, size_t j);
+size_t lomuto_partition(int *array, size_t low, size_t high);
 void quick_sort_rec(int *array, size_t low, size_t high);
 
 #endif /* SORT_H */
