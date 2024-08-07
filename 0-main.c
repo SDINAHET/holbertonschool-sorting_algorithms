f7b7ff7162ae164b17f711f759b5d619782c2784
SDINAHET <stephane.dinahet@gmail.com>
2024-08-07 14:12:04 +0200

    ajout comment task 0

--- 0-main.c
+++ 0-main.c
@@ -12,10 +12,10 @@ int main(void)
 	int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
 	size_t n = sizeof(array) / sizeof(array[0]);
 
-	print_array(array, n);
+	print_array(array, n); /*1er array 19, 48, 99, 71, 13, 52, 96, 73, 86, 7*/
 	printf("\n");
-	bubble_sort(array, n);
+	bubble_sort(array, n); /*trie bubble en array*/
 	printf("\n");
-	print_array(array, n);
+	print_array(array, n); /*2ème array 7, 13, 19, 48, 52, 71, 73, 86, 96, 99*/
 	return (0);
 }
