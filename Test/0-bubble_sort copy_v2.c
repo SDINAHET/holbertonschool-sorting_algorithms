e012f9580c6e72467223b8f9e89ca826197430bb
SDINAHET <stephane.dinahet@gmail.com>
2024-08-07 14:00:50 +0200

    fix bubble

--- Test/0-bubble_sort copy_v2.c
+++ Test/0-bubble_sort copy_v2.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 #include "sort.h"
+
+/**
+ * print_array - Prints an array of integers.
+ * @array: The array to print.
+ * @size: The size of the array.
+ */
+void print_array(int *array, size_t size)
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", array[i]);
+	}
+	printf("\n");
+}
+
 /**
  * bubble_sort - Sorts an array of integers in ascending order using
  *                the Bubble sort algorithm.
