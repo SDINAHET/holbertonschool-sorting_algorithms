f6bbae58b347667e6c075124dba856feb98e3142
SDINAHET <stephane.dinahet@gmail.com>
2024-08-08 13:14:49 +0200

    select quick

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -6,7 +6,7 @@
  * @array: The array to sort.
  * @size: The size of the array.
  */
-void selection_sort(int *array, size_t size) void selection_sort(int *array, size_t size)
+void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min_index;
 
